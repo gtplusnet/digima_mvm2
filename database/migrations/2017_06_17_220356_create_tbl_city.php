27c0f9706f8cebbdea7a10253675169984764329
skusztacles <rm.shental@gmail.com>
2017-06-23 07:35:13 +0800

    RenzRegistration

---
+++ database/migrations/2017_06_17_220356_create_tbl_city.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTblCity extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_city', function (Blueprint $table) {
+            $table->increments('city_id');
+            $table->string('city_name');
+            $table->integer('postal_code');
+            $table->integer('county_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tbl_city');
+    }
+}
