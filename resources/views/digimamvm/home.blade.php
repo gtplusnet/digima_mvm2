4e98e19e97fa237f2b802da8f1f8627a92614140
Your Name <you@example.com>
2017-06-23 07:43:47 -0700

    renz

---
+++ resources/views/digimamvm/home.blade.php
@@ -0,0 +1,51 @@
+@extends('digimamvm.layout')
+@section('content')
+<div class="container">
+	<p class="categories-text">CATEGORIES</p>
+</div>
+<div class="category-layer">
+	<div class="container">
+		<table>
+			<tbody>
+				<tr>
+					<td>
+						<button href="#"  class="btn btn-xlarge" /><i class="material-icons">restaurant</i><p>Restaurant</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-hotel fa-5x"></i><p>Hotels</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-automobile fa-5x"></i><p>Automotive</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="material-icons">nature</i><p>Beauty & Spa</p></button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<button href="#"  class="btn btn-xlarge" /><i class="material-icons">restaurant</i><p>Restaurant</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-hotel fa-5x"></i><p>Hotels</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-automobile fa-5x"></i><p>Automotive</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="material-icons">nature</i><p>Beauty & Spa</p></button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<button href="#"  class="btn btn-xlarge" /><i class="material-icons">restaurant</i><p>Restaurant</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-hotel fa-5x"></i><p>Hotels</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-automobile fa-5x"></i><p>Automotive</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="material-icons">nature</i><p>Beauty & Spa</p></button>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<button href="#"  class="btn btn-xlarge" /><i class="material-icons">restaurant</i><p>Restaurant</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-hotel fa-5x"></i><p>Hotels</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="fa fa-automobile fa-5x"></i><p>Automotive</p></button>
+	              		<button href="#"  class="btn btn-xlarge" /><i class="material-icons">nature</i><p>Beauty & Spa</p></button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<div class="background-features-theme" style="background-image: url('/images/background_features.jpg')">
+	<div class="container">
+		<p class="featured-text"><font class="featured-bold">FEATURED</font> LISTING</p>	
+	</div>
+</div>
+@endsection
