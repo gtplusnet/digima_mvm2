47e403b360f87c466566905924fb667f6a5e074c
skusztacles <rm.shental@gmail.com>
2017-06-23 14:40:04 +0800

    BusinessCategoryMigration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
-            $this->call(CityTableSeeder::class);
-            $this->call(CountyTableSeeder::class);
+        $this->call(CityTableSeeder::class);
+        $this->call(CountyTableSeeder::class);
+        $this->call(BusinessCategoryTableSeeder::class);
 
         Model::reguard();
     }
