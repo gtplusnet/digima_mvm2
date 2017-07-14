b176dc104084d452802561db7152b5ffd13226ad
Your Name <you@example.com>
2017-07-14 15:03:52 -0700

    contact

--- resources/views/front/layout/layout.blade.php
+++ resources/views/front/layout/layout.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
         <!-- Title -->
         <title>CROTIA Directory | {{ $page }}</title>
         
@@ -131,6 +130,9 @@
         <script src="/initializr/js/vendor/bootstrap.min.js"></script>
         <script>window.jQuery || document.write('<script src="/initializr/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
         <script src="/initializr/js/vendor/bootstrap.min.js"></script>
+        <!-- FOR GOOGLEMAP -->
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDoOPN-LMZZYOB3qYn3AcQV3ITZU7tuUQ&callback=initMap">
+        </script>
        <!--  <script type="text/javascript" scr="/assets/js/front/rangeslider.js"></script> -->
 
         <!-- HANGGANG DITO -->
