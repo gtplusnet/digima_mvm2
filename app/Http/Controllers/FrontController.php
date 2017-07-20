45d57406e194bccda707b70cde6511f76fca59f6
Your Name <you@example.com>
2017-07-19 18:16:05 -0700

    login baka mag-ERROR

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -63,32 +63,33 @@ class FrontController extends Controller
         }
         else
         {
-        $business_data = new TblBusinessModel;
-        $business_data->business_id = '';
-        $business_data->business_name = $request->business_name;
-        $business_data->city_id = $request->city_list;
-        $business_data->business_complete_address = $request->business_address;
-        $business_data->business_phone = $request->primary_business_phone;
-        $business_data->business_alt_phone = $request->secondary_business_phone;
-        $business_data->business_fax = $request->fax_number;
-        $business_data->facebook_url = $request->facebook_url;
-        $business_data->twitter_url = $request->twitter_username;
-        $business_data->date_created = Carbon::now();
+	        $business_data = new TblBusinessModel;
+	        $business_data->business_id = '';
+	        $business_data->business_name = $request->business_name;
+	        $business_data->city_id = $request->city_list;
+	        $business_data->business_complete_address = $request->business_address;
+	        $business_data->business_phone = $request->primary_business_phone;
+	        $business_data->business_alt_phone = $request->secondary_business_phone;
+	        $business_data->business_fax = $request->fax_number;
+	        $business_data->facebook_url = $request->facebook_url;
+	        $business_data->twitter_url = $request->twitter_username;
+	        $business_data->date_created = Carbon::now();
 
-        $business_data->save();
+	        $business_data->save();
 
-        $contact_data = new TblBusinessContactPersonModel;
-        $contact_data->business_contact_person_id = '';
-        $contact_data->contact_prefix = $request->prefix;
-        $contact_data->contact_first_name = $request->first_name;
-        $contact_data->contact_last_name = $request->last_name;
-        $contact_data->business_id = $business_data->business_id;
+	        $contact_data = new TblBusinessContactPersonModel;
+	        $contact_data->business_contact_person_id = '';
+	        $contact_data->contact_prefix = $request->prefix;
+	        $contact_data->contact_first_name = $request->first_name;
+	        $contact_data->contact_last_name = $request->last_name;
+	        $contact_data->business_id = $business_data->business_id;
+    	}
+	}
 
     public function payment()
     {
         $data['page']   = 'payment';
         return view('front.pages.payment', $data);
-    }
 
         $contact_data->save();
 
@@ -103,7 +104,7 @@ class FrontController extends Controller
         $account_data->save();
 
         echo 'Registered successfully ! But your account is pending.';
-        }   
+  
     }
 
     public function get_postal_code(Request $request)
