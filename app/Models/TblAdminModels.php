e65dbab11c498744321fdd0f8adac7e37bd526fb
james <jamesomosora@gmail.com>
2017-10-28 19:26:29 +0800

    james

--- app/Models/TblAdminModels.php
+++ app/Models/TblAdminModels.php
@@ -3,15 +3,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class TblAgentModels extends Model
-{
-	protected $table = 'tbl_agent';
-    protected $primaryKey = 'agent_id';
-    public $timestamps = false;
 
-    public function scopeTeam($query)
-    {
-    	$query->join('tbl_team', 'tbl_team.team_id', '=', 'tbl_agent.team_id');
-    	return $query;
-    }
+class TblAdminModels extends Model
+
+{
+	protected $table = 'tbl_admin';
+    protected $primaryKey = 'admin_id';
+    public $timestamps = false;
 }
