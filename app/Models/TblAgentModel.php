50c5b1614f87271f6b8dc194e13cbec240087386
james <jamesomosora@gmail.com>
2017-10-28 19:19:01 +0800

    james

--- app/Models/TblAgentModel.php
+++ app/Models/TblAgentModel.php
@@ -9,9 +9,5 @@ class TblAgentModel extends Model
     protected $primaryKey = 'agent_id';
     public $timestamps = false;
 
-    public function scopeTeam($query)
-    {
-    	$query->join('tbl_team', 'tbl_team.team_id', '=', 'tbl_agent.team_id');
-    	return $query;
-    }
+    
 }
