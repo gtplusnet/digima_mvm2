5cdcf867d2a66d1e15fbac6b8a4e178665a30f62
james <jamesomosora@gmail.com>
2017-11-15 18:02:27 +0800

    james

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -78,7 +78,7 @@ class FrontController extends Controller
         $data['_categories']        = TblBusinessCategoryModel::where('parent_id',0)->get();
         $data['_most_viewed']       = TblReportsModel::join('tbl_business','tbl_business.business_id','=','tbl_reports.business_id')
                                     ->join('tbl_business_images','tbl_business_images.business_id','=','tbl_business.business_id')
-                                    ->orderBy('tbl_reports.business_views','ASC')
+                                    ->orderBy('tbl_reports.business_views','DESC')
                                     ->limit(4)
                                     ->get();
         return view('front.pages.home',$data);
@@ -152,8 +152,7 @@ class FrontController extends Controller
         $data['_categories']        = TblBusinessCategoryModel::where('parent_id',$request->parent_id)->get();
         $data['_most_viewed']       = TblReportsModel::join('tbl_business','tbl_business.business_id','=','tbl_reports.business_id')
                                     ->join('tbl_business_images','tbl_business_images.business_id','=','tbl_business.business_id')
-                                    ->groupBy('tbl_business.business_id')
-                                    ->orderBy('tbl_reports.business_views','ASC')
+                                    ->orderBy('tbl_reports.business_views','DESC')
                                     ->limit(4)
                                     ->get();
         
@@ -316,8 +315,7 @@ class FrontController extends Controller
         $data['_categories']        = TblBusinessCategoryModel::where('parent_id',0)->get();
         $data['_most_viewed']       = TblReportsModel::join('tbl_business','tbl_business.business_id','=','tbl_reports.business_id')
                                     ->join('tbl_business_images','tbl_business_images.business_id','=','tbl_business.business_id')
-                                    ->groupBy('tbl_business.business_id')
-                                    ->orderBy('tbl_reports.business_views','ASC')
+                                    ->orderBy('tbl_reports.business_views','DESC')
                                     ->limit(4)
                                     ->get();
         return view('front.pages.searchresult',$data); 
