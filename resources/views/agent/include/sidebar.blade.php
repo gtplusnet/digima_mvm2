dfeef2cd37cd34d60ba1943913162e8845fc47e3
james <jamesomosora@gmail.com>
2018-04-23 17:54:06 +0800

    james omosora

--- resources/views/agent/include/sidebar.blade.php
+++ resources/views/agent/include/sidebar.blade.php
@@ -6,7 +6,7 @@
             <li class="{{ (Request::segment(2)  == 'profile') ? 'active' : ''  }}"><a href="/agent/profile" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-user"></span><p>PROFILE</p></a></li>
 			<li class="{{ (Request::segment(2)  == 'merchant') ? 'active' : ''  }}"><a href="/agent/merchant" class="waves-effect waves-button"><span class="menu-icon fa fa-group"></span><p>MERCHANT</p></a></li>
             <li class="{{ (Request::segment(2)  == 'add') ? 'active' : ''  }}"><a href="/agent/add/merchant" class="waves-effect waves-button"><span class="menu-icon fa fa-user-plus"></span><p>ADD MERCHANT</p></a></li>
-			<li style="display:none;" class="mob-logout"><a href="/agent/logout" class="waves-effect waves-button"><span class="menu-icon fa fa-sign-out"></span><p>SIGN OUT</p></a></li>	
+			<li style="display:none;" class="mob-logout"><a href="/user/logout" class="waves-effect waves-button"><span class="menu-icon fa fa-sign-out"></span><p>SIGN OUT</p></a></li>	
         </ul>
     </div>
 </div>
--- resources/views/supervisor/include/sidebar.blade.php
+++ resources/views/supervisor/include/sidebar.blade.php
@@ -6,7 +6,7 @@
             <li class="{{ (Request::segment(2) == 'profile') ? 'active' : ''  }}"><a href="/supervisor/profile" class="waves-effect waves-button"><span class="menu-icon glyphicon glyphicon-user"></span><p>Profile</p></a></li>
             <li class="{{ (Request::segment(2) == 'merchant') ? 'active' : ''  }}"><a href="/supervisor/merchant" class="waves-effect waves-button"><span class="menu-icon fa fa-group"></span><p>Merchants</p></a></li>
             <li class="{{ (Request::segment(2) == 'manage_user') ? 'active' : ''  }}"><a href="/supervisor/manage_user" class="waves-effect waves-button"><span class="menu-icon fa fa-user-plus"></span><p>Manage Team/Agent</p></a></li>
-        	<li style="display:none;" class="mob-logout"><a href="/supervisor/logout" class="waves-effect waves-button"><span class="menu-icon fa fa-sign-out"></span><p>Sign Out</p></a></li>	
+        	<li style="display:none;" class="mob-logout"><a href="/user/logout" class="waves-effect waves-button"><span class="menu-icon fa fa-sign-out"></span><p>Sign Out</p></a></li>	
         </ul>
     </div>
 </div>
